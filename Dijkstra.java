7aa55cde18f3e472d17547d3b810dd6a675b338b
Marco Sartori <marco.sartori@fag.edu.br>
2023-08-12 16:20:04 -0300

    Adição de outros comentários.

--- Dijkstra.java
+++ Dijkstra.java
@@ -25,8 +25,8 @@ public class ShortestDistanceFinder {
         cityGraph.addEdge("C", "D", 2);
         cityGraph.addEdge("D", "E", 7);
 
-        String startCity = "A";
-        String endCity = "E";
+        String startCity = "A"; // Cidade de origem
+        String endCity = "E"; // Cidade de destino
 
         int shortestDistance = dijkstra(cityGraph, startCity, endCity);
         System.out.println("A menor distância entre " + startCity + " e " + endCity + " é: " + shortestDistance);
